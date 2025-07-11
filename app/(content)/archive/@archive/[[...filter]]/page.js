18101074113b8fd1876b7dd7389323a942aaafbe
MaestroOG <muneeburrehman822@gmail.com>
2025-07-11 16:07:57 +0500

    initial commit

---
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,65 @@
+import NewsList from '@/components/NewsList'
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from '@/lib/news'
+import Link from 'next/link';
+import React from 'react'
+
+const FilteredNews = ({ params }) => {
+    const filter = params.filter;
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
+
+    let news;
+    let links = getAvailableNewsYears();
+
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear)
+        links = getAvailableNewsMonths(selectedYear)
+    }
+
+    if (selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = []
+    }
+
+
+    let newsContent = <p>No time period selected yet.</p>
+
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />
+    }
+
+    if (
+        (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+        (selectedYear && selectedMonth && !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+    ) {
+        throw new Error(`Invalid Filter`);
+
+    }
+
+    return (
+        <>
+            <header id="archive-header">
+                <nav>
+                    <ul>
+                        {links.map(link => {
+
+                            const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`
+
+                            return (
+                                (
+                                    <li key={link}>
+                                        <Link href={href}>{link}</Link>
+                                    </li>
+                                )
+                            )
+                        })}
+                    </ul>
+                </nav>
+            </header>
+
+            {newsContent}
+        </>
+    )
+}
+
+export default FilteredNews
---
+++ app/api/route.js
@@ -0,0 +1,5 @@
+export function GET(request) {
+    console.log(request)
+
+    return Response.json({ message: "This is a placeholder response for the API route." })
+}
